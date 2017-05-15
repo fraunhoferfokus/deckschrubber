5434c7c5acbb3a96518991526dfa340a39989f9a
Yan Foto <yan.foto@quaintous.com>
2017-05-15 13:42:28 +0200

    Add first draft
    - Only for insecure registries
    - Can filter images by age (year, month, day, or any combination)

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"sort"
+	"time"
+
+	"encoding/json"
+
+	"flag"
+
+	"io"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/distribution/context"
+	schema2 "github.com/docker/distribution/manifest/schema2"
+	"github.com/docker/distribution/reference"
+	"github.com/docker/distribution/registry/client"
+)
+
+var (
+	/** CLI flags */
+	// Base URL of registry
+	registryURL *string
+	// Maximum age of image to consider for deletion
+	day, month, year *int
+	// Max number of repositories to be fetched from registry
+	repoCount *int
+)
+
+func init() {
+	/** CLI flags */
+	// Max number of repositories to fetch from registry (default = 5)
+	repoCount = flag.Int("repos", 5, "number of repositories to garbage collect")
+	// Base URL of registry (default = http://localhost:5000)
+	registryURL = flag.String("registry", "http://localhost:5000", "URL of registry")
+	// Maximum age of iamges to consider for deletion in days (default = 0)
+	day = flag.Int("day", 0, "max age in days")
+	// Maximum age of months to consider for deletion in days (default = 0)
+	month = flag.Int("month", 0, "max age in months")
+	// Maximum age of iamges to consider for deletion in years (default = 0)
+	year = flag.Int("year", 0, "max age in days")
+}
+
+func main() {
+	flag.Parse()
+
+	// Create registry object
+	r, err := client.NewRegistry(context.Background(), *registryURL, nil)
+	if err != nil {
+		log.Fatalf("Could not create registry object! (err: %s", err)
+	}
+
+	// List of all repositories fetched from the registry. The number
+	// of fetched repositories depends on the number provided by the
+	// user ('-repos' flag)
+	entries := make([]string, *repoCount)
+
+	// Empty context for all requests in sequel
+	ctx := context.Background()
+
+	// Fetch all repositories from the registry
+	numFilled, err := r.Repositories(ctx, entries, "")
+	if err != nil && err != io.EOF {
+		log.Fatalf("Error while fetching repositories! (err: %v)", err)
+	}
+	log.WithFields(log.Fields{"count": numFilled, "entries": entries}).Info("Successfully fetched repositories.")
+
+	repoImages := make(map[string][]Image)
+
+	// Fetch information about images belonging to each repository
+	for _, entry := range entries[:numFilled] {
+		logger := log.WithField("repo", entry)
+
+		// Establish repository object in registry
+		repoName, _ := reference.ParseNamed(entry)
+		repo, err := client.NewRepository(context.Background(), repoName, *registryURL, nil)
+		if err != nil {
+			logger.WithFields(log.Fields{"entry": entry}).Fatalf("Could not create repo from name! (err: %v)", err)
+		}
+		logger.Debug("Successfully created repository object.")
+
+		tagsService := repo.Tags(ctx)
+		blobsService := repo.Blobs(ctx)
+		manifestService, err := repo.Manifests(ctx)
+		if err != nil {
+			logger.Fatalf("Couldn't fetch manifest service! (err: %v)", err)
+		}
+
+		tags, err := tagsService.All(ctx)
+		if err != nil {
+			logger.Fatalf("Couldn't fetch tags! (err: %v)", err)
+		}
+
+		var images []Image
+
+		// Fetch information about each tag of a repository
+		// This involves fetching the manifest, its details,
+		// and the corresponding blob information
+		for _, tag := range tags {
+			tagLogger := logger.WithField("tag", tag)
+
+			tagLogger.Debug("Fetching tag...")
+			desc, err := tagsService.Get(ctx, tag)
+			if err != nil {
+				tagLogger.Error("Could not fetch tag!")
+				continue
+			}
+
+			tagLogger.Debug("Fetching manifest...")
+			mnfst, err := manifestService.Get(ctx, desc.Digest)
+			if err != nil {
+				tagLogger.Error("Could not fetch manifest!")
+				continue
+			}
+
+			tagLogger.Debug("Parsing manifest details...")
+			_, p, err := mnfst.Payload()
+			if err != nil {
+				tagLogger.Error("Could not parse manifest detail!")
+				continue
+			}
+
+			m := new(schema2.DeserializedManifest)
+			m.UnmarshalJSON(p)
+
+			tagLogger.Debug("Fetching blob")
+			b, err := blobsService.Get(ctx, m.Manifest.Config.Digest)
+			if err != nil {
+				tagLogger.Error("Could not fetch blob!")
+				continue
+			}
+
+			var blobInfo BlobInfo
+			json.Unmarshal(b, &blobInfo)
+
+			images = append(images, Image{entry, tag, blobInfo.Created, func() error { return manifestService.Delete(context.Background(), desc.Digest) }})
+		}
+
+		sort.Sort(ImageByDate(images))
+		repoImages[entry] = images
+	}
+
+	// Deadline defines the youngest creation date for an image
+	// to be considered for deletion
+	deadline := time.Now().AddDate(*year/-1, *month/-1, *day/-1)
+
+	for name, tags := range repoImages {
+		logger := log.WithField("repo", name)
+
+		logger.Infof("Analyzing tags...")
+
+		tagCount := len(tags)
+		if tagCount <= 1 {
+			continue
+		}
+
+		for _, tag := range tags[:tagCount-1] {
+			if tag.Time.Before(deadline) {
+				logger.WithField("tag", tag.Tag).Info("Trying to delete outdated image found...")
+
+				err := tag.Delete()
+				if err != nil {
+					logger.WithField("tag", tag.Tag).Error("Could not delete image!")
+				}
+			}
+		}
+	}
+}
---
+++ types.go
@@ -0,0 +1,24 @@
+package main
+
+import "time"
+
+// BlobInfo represents information about a specific Blob
+type BlobInfo struct {
+	Created time.Time `json:"created"` // Creation time
+}
+
+// Image represents a docker image with a specific tag
+type Image struct {
+	Repository string       // Name of repository to which image belongs
+	Tag        string       // Image's tag
+	Time       time.Time    // Creation time of the image
+	Delete     func() error // Function to delete image from registry
+}
+
+// ImageByDate represents an array of images
+// sorted by creation date
+type ImageByDate []Image
+
+func (ibd ImageByDate) Len() int           { return len(ibd) }
+func (ibd ImageByDate) Swap(i, j int)      { ibd[i], ibd[j] = ibd[j], ibd[i] }
+func (ibd ImageByDate) Less(i, j int) bool { return ibd[i].Time.Before(ibd[j].Time) }
