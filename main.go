a146da92a0f159ac91d24297a16c4f792c00e16c
Yan Foto <yan.foto@quaintous.com>
2018-02-03 21:01:39 +0100

    Preliminary version of basic auth.
    NOTE: this is only for testing and is not production ready.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"sort"
 	"strings"
+	"syscall"
 	"time"
 
 	"encoding/json"
@@ -16,11 +17,14 @@ import (
 
 	"regexp"
 
+	"golang.org/x/crypto/ssh/terminal"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/distribution/context"
 	schema2 "github.com/docker/distribution/manifest/schema2"
 	"github.com/docker/distribution/reference"
 	"github.com/docker/distribution/registry/client"
+	"github.com/fraunhoferfokus/deckschrubber/util"
 )
 
 var (
@@ -44,6 +48,10 @@ var (
 
 	// Compiled regexps
 	repoRegexp, tagRegexp, negTagRegexp *regexp.Regexp
+	// Skip insecure TLS
+	insecure *bool
+	// Username and password
+	uname, passwd *string
 )
 
 const (
@@ -76,6 +84,11 @@ func init() {
 	dry = flag.Bool("dry", false, "does not actually deletes")
 	// Shows version
 	ver = flag.Bool("v", false, "shows version and quits")
+	// Skip insecure TLS
+	insecure = flag.Bool("insecure", false, "Skip insecure TLS verification")
+	// Username and password
+	uname = flag.String("user", "", "Username for basic authentication")
+	passwd = flag.String("password", "", "Password for basic authentication")
 }
 
 func main() {
@@ -97,8 +110,22 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
+	// Add basic auth if user/pass is provided
+	if *uname != "" && *passwd == "" {
+		fmt.Println("Password:")
+		bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err == nil {
+			stringPassword := string(bytePassword[:])
+			passwd = &stringPassword
+		} else {
+			fmt.Println("Could not read password. Quitting!")
+			os.Exit(1)
+		}
+	}
+	basicAuthTransport := util.NewBasicAuthTransport(*registryURL, *uname, *passwd, *insecure)
+
 	// Create registry object
-	r, err := client.NewRegistry(*registryURL, nil)
+	r, err := client.NewRegistry(*registryURL, basicAuthTransport)
 	if err != nil {
 		log.Fatalf("Could not create registry object! (err: %s", err)
 	}
